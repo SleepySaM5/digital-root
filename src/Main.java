a1d756087feca0ff5306ede1c02e189e189cf0c6
SleepySaM5 <vtibii@gmail.com>
2023-02-14 01:32:14 +0100

    Added Input to expect number, added loop to print digits of a number

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner inputNumber = new Scanner(System.in);
+        System.out.println("Calculate the digital root of: ");
+        int numberToDigitize = inputNumber.nextInt();
+
+        if(numberToDigitize < 0) {
+            numberToDigitize *= -1;
+        }
+
+        System.out.println("Digits of the input number: ");
+        while(numberToDigitize != 0) {
+            int digit = numberToDigitize % 10;
+            System.out.println(digit + " ");
+            numberToDigitize /= 10;
+        }
+
+        // System.out.println("Digital root of " + numberToDigitize + " is the following: ");
     }
 }
