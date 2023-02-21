e8cf01f671d092749fa2b5974c07e4bb5edd5f6f
SleepySaM5 <vtibii@gmail.com>
2023-02-21 01:24:57 +0100

    Added function to calculate Digital Root of input number

--- src/Main.java
+++ src/Main.java
@@ -6,37 +6,38 @@ public class Main {
      * @param n A positive integer.
      * @return The number of digits of the input number.
      */
-    static int countNumberOfDigits(int n) {
-        int numberOfDigits = 1;
+    static int countDigits(int n) {
+        if(n == 0) {
+            return 1;
+        }
+        int numberOfDigits = 0;
         
-        while(n > 9) {
+        while(n != 0) {
             n /= 10;
             numberOfDigits++;
         }
 
         return numberOfDigits;
     }
+    static int sumOfDigits = 0;
+
+    static int calculateSumOfNumber(int n) {
+        if(n == 0)
+            return 0;
+        return n % 10 + calculateSumOfNumber(n/10);
+    }
+
+    static int calculateDigitalRoot(int n) {
+        if(n == 0)
+            return 0;
+        int digitalRoot = n % 10 + calculateDigitalRoot(n / 10);
+        if(countDigits(digitalRoot) > 1) {
+            return calculateDigitalRoot(digitalRoot);
+        }
+        return digitalRoot;
+    }
 
     public static void main(String[] args) {
-        int sumOfDigits = 0;
-        Scanner inputNumber = new Scanner(System.in);
-        System.out.println("Calculate the digital root of: ");
-        int numberToDigitize = inputNumber.nextInt();
-
-        if(numberToDigitize < 0) {
-            numberToDigitize *= -1;
-        }
-
-        System.out.println("Digits of the input number: ");
-        while(numberToDigitize != 0) {
-            int digit = numberToDigitize % 10;
-            sumOfDigits += digit;
-            System.out.println(digit + " ");
-            numberToDigitize /= 10;
-        }
-
-        System.out.println(sumOfDigits);
-        System.out.println("number of digits: " + countNumberOfDigits(1234567));
-        // System.out.println("Digital root of " + numberToDigitize + " is the following: ");
+        System.out.println(calculateDigitalRoot(5836734));
     }
 }
